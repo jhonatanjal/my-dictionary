3d9bba2d0bffbd92c33c59e81d0981df5f83c71d
Jhonatan Aguiar Lopes <jhonatan.jal@gmail.com>
2018-09-23 21:13:22 -0300

    initial commit

---
+++ app/src/main/java/com/jhonatanlopes/mydictionary/DictionaryActivity.kt
@@ -0,0 +1,12 @@
+package com.jhonatanlopes.mydictionary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DictionaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dictionary)
+    }
+}
