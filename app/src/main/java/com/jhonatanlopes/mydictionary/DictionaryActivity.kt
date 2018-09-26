da792471bdc511ad0ca2569d9e01d866e4ec1b85
Jhonatan Aguiar Lopes <jhonatan.jal@gmail.com>
2018-09-26 00:04:54 -0300

    add search menu

--- app/src/main/java/com/jhonatanlopes/mydictionary/DictionaryActivity.kt
+++ app/src/main/java/com/jhonatanlopes/mydictionary/DictionaryActivity.kt
@@ -1,12 +1,42 @@
 package com.jhonatanlopes.mydictionary
 
-import android.support.v7.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.widget.SearchView
+import kotlinx.android.synthetic.main.activity_dictionary.*
 
 class DictionaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dictionary)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        handleIntent(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setIconifiedByDefault(false)
+            requestFocus()
+        }
+        return true
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (intent.action == Intent.ACTION_SEARCH) {
+            val word = intent.getStringExtra(SearchManager.QUERY)
+            tv_hello_word.text = word
+        }
     }
 }
